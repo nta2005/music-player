f0f34f62d147645630d017a2cbd7a05dc16e476a
nta2005 <ntapro1412@gmail.com>
2022-10-13 17:16:33 +0700

    add project

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const songs = [
+  {
+    name: "Chúng Ta Của Hiện Tại",
+    singer: "Sơn Tùng M-TP",
+    path: "./assets/music/song1.mp3",
+    img: "./assets/img/song1.jpg",
+  },
+
+  {
+    name: "Có Chắc Yêu Là Đây",
+    singer: "Sơn Tùng M-TP",
+    path: "./assets/music/song2.mp3",
+    img: "./assets/img/song2.jpg",
+  },
+
+  {
+    name: "Có Ai Đâu Ngờ",
+    singer: "Thu Cầm",
+    path: "./assets/music/song3.mp3",
+    img: "./assets/img/song3.jpg",
+  },
+
+  {
+    name: "Muộn Rồi Mà Sao Còn",
+    singer: "Sơn Tùng M-TP",
+    path: "./assets/music/song4.mp3",
+    img: "./assets/img/song4.jpg",
+  },
+
+  {
+    name: "Người Yêu Giản Đơn",
+    singer: "Chi Dân",
+    path: "./assets/music/song5.mp3",
+    img: "./assets/img/song5.jpg",
+  },
+];
